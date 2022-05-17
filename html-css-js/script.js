68c02256889971a2b36b459793bfa8b3d54a27e4
Atilhan <gorinchem1995@hotmail.com>
2022-05-17 12:30:59 +0200

    Finished

---
+++ html-css-js/script.js
@@ -0,0 +1,18 @@
+//var btn = document.createElement('button');
+btn.style.margin = '10px';
+btn.innerHTML = '...';
+document.body.appendChild(btn);
+
+// schijf hier tussen je code
+
+
+function switch_on(){
+    document.getElementById('img').src='lamp_aan.png';
+}
+
+function switch_off(){
+    document.getElementById('img').src='lamp_uit.png';
+}
+
+
+// schijf hier tussen je code
